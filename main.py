7a25cb7e723c533caad7009ef6265f0068beca50
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-27 00:36:40 -0800

    added main function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World I'm tynra code a s[eed test!!!")
+# Allows styling of the terminal
+import curses
+#initallizes curses module which helps runs different commands from it.
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Hello World I'm tynra code a speed test!!!")
+    stdscr.refresh()
+    # getkey() helps to not immediately close the program.
+    stdscr.getkey()
+
+#Calls function
+wrapper(main)
