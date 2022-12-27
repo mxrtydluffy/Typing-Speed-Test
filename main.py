c468796493a898068eedb34854b7b679f9b310a8
mxrtydluffy <marty.marinda@students.dominican.edu>
2022-12-27 15:16:40 -0800

    added code to wpm

--- main.py
+++ main.py
@@ -12,11 +12,44 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    target_text = "Hello world lets type some text! Thye cow says moo."
+    current_text = []
+
+    stdscr.getkey()
+
+    # Waits for user to type something then it append to the current text.
+    while True:
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        # Then loop through every character/ keys in a for loop the user types by storing it in a list.
+        # Then display the charater on the screen 
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+        stdscr.refresh()
+
+        key = stdscr.getkey()
+
+        # In ASCII 27 is the number for escape
+        if ord(key) == 27:
+            break
+
+        current_text.append(key)
+
+
+        # Helps clear the screen because if it doesn't it will repeat the text a ton of times.
+        # Not clearing what the previous text says.
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        
+
 def main(stdscr):
 
     """
     STD screen aka standaard output which is the terminal
-    since its where we're wiritng stuffy out to.
+    since its where we're writing stuff out to.
     - std.scr.clear() clears the entire screen
     - stdscr.addstr adds the string printed out to the console.
     - stdscr.refresh() once you start typing need to refresh the screen.
@@ -29,6 +62,7 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 #Calls function
 wrapper(main)
